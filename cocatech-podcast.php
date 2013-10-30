86577b69c751d419905b23eb2a779f4f19c11370
Joel Garcia Jr <joel.garciajr84@gmail.com>
2013-10-30 14:38:35 -0200

    adicionando arquivos

---
+++ cocatech-podcast.php
@@ -0,0 +1,14 @@
+<?php 
+/*
+* Plugin Name: COCATECH - PODCAST
+* Plugin URL: http://www.joelgarciajr.com.br
+* Description: Cria um Widget para exibição dos 5 ultimos episódios
+* Author: @joelgarciajr84
+* Version: 1.0
+* Author URL: http://www.joelgarciajr.com.br
+*/
+?>
+<?php 
+	require_once dirname( __FILE__ ) . '/rss-import/rssimport.php';#Activate rss import plugin
+	require_once dirname( __FILE__ ) . '/widget.php';#Call Widget file
+?>
